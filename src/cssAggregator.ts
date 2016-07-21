a205d1967f6808c2c0416fb0c9696cadbed86454
Anders E. Andersen <andersa@ellenshoej.dk>
2016-07-21 19:53:52 +0200

    Fix for issue #1

--- src/cssAggregator.ts
+++ src/cssAggregator.ts
@@ -1,6 +1,6 @@
 import { workspace } from 'vscode'
 import { parse, Rule } from 'css';
-import * as path from 'path'
+import { readFile } from 'fs'
 
 function flatten<T>(nestedArray: T[][]): T[] {
     if (nestedArray.length === 0) {
@@ -29,49 +29,53 @@ function findClassName(selector: string): string {
 export default function () {
 
     return workspace.findFiles('**/*.css', '').then(uris => {
-
-        let cssTextPromises = uris.map(
-            uri => workspace.openTextDocument(uri).then(cssDocument => {
-                console.log("Parsing - " + cssDocument.fileName);
-
-                return cssDocument.getText();
-            }, console.log)
+        
+        let cssTextPromises = uris.map(uri =>
+            <PromiseLike<string>>new Promise<string>((resolve, reject) =>
+                readFile(uri.fsPath, workspace.getConfiguration('files').get('encoding'), (err, data) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data.toString());
+                    }
+                })
+            )
         );
 
-        let cssASTPromises = cssTextPromises.map(
-            cssTextPromise => cssTextPromise.then(cssText => parse(cssText), console.log)
-        );
+    let cssASTPromises = cssTextPromises.map(
+        cssTextPromise => cssTextPromise.then(cssText => parse(cssText), console.log)
+    );
 
-        let rulesPromises = cssASTPromises.map(
-            cssASTPromise => cssASTPromise.then(cssAST =>
-                <Rule[]>(cssAST.stylesheet.rules.filter(node => (<Rule>node).type === 'rule')),
-                console.log)
-        );
+    let rulesPromises = cssASTPromises.map(
+        cssASTPromise => cssASTPromise.then(cssAST =>
+            <Rule[]>(cssAST.stylesheet.rules.filter(node => (<Rule>node).type === 'rule')),
+            console.log)
+    );
 
-        let selectorsPromises = rulesPromises.map(
-            rulesPromise => rulesPromise.then(rules => {
-                if (rules.length > 0) {
-                    return flatten(rules.map(rule => rule.selectors));
-                } else {
-                    return [];
-                }
-            }, console.log)
-        );
+    let selectorsPromises = rulesPromises.map(
+        rulesPromise => rulesPromise.then(rules => {
+            if (rules.length > 0) {
+                return flatten(rules.map(rule => rule.selectors));
+            } else {
+                return [];
+            }
+        }, console.log)
+    );
 
-        let cssClassesPromises = selectorsPromises.map(
-            selectorsPromise => selectorsPromise.then(selectors =>
-                selectors.map(selector => findClassName(selector)).filter(value => value !== ""),
-                console.log)
-        );
+    let cssClassesPromises = selectorsPromises.map(
+        selectorsPromise => selectorsPromise.then(selectors =>
+            selectors.map(selector => findClassName(selector)).filter(value => value !== ""),
+            console.log)
+    );
 
-        return Promise.all(cssClassesPromises).then(cssClassesInAllFiles => {
-            let allcssClasses = Array.from(new Set(
-                cssClassesInAllFiles
-                    .filter(cssClasses => cssClasses.length > 0)
-                    .reduce((p, c) => p.concat(c))
-            ));
-
-            return allcssClasses;
-        }, console.log);
+    return Promise.all(cssClassesPromises).then(cssClassesInAllFiles => {
+        let allcssClasses = Array.from(new Set(
+            cssClassesInAllFiles
+                .filter(cssClasses => cssClasses.length > 0)
+                .reduce((p, c) => p.concat(c))
+        ));
+       
+        return allcssClasses;
     }, console.log);
+}, console.log);
 }
