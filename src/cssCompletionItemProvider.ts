e86a6fcf7557eefbf1e7aec0e31cc7a3a60df78a
Anders E. Andersen <andersa@ellenshoej.dk>
2016-09-02 16:24:44 +0200

    Refactor provider into typescript class.
    In preparation for adding ability to refresh the css class list, refactor the CompletionItemProvider into a typescript class.

--- src/cssCompletionItemProvider.ts
+++ src/cssCompletionItemProvider.ts
@@ -2,15 +2,15 @@
 import * as vscode from 'vscode';
 import aggregator from './cssAggregator';
 
-let completionItems = aggregator().then(cssClasses => cssClasses.map(cssClass => new vscode.CompletionItem(cssClass)));
+export class CssCompletionItemProvider implements vscode.CompletionItemProvider {
+    private completionItems = aggregator().then(cssClasses => cssClasses.map(cssClass => new vscode.CompletionItem(cssClass)));
 
-export default {
-    provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.CompletionItem[]> {
+    public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.CompletionItem[]> {
         let lineUntilPosition = document.getText(new vscode.Range(position.with(undefined, 0), position));
         let textAfterClassAttributeStart = lineUntilPosition.substr(lineUntilPosition.lastIndexOf('class='));
         let attributeClosed = textAfterClassAttributeStart.search(/class=(?:\"[a-zA-Z0-9-\s]*\"|\'[a-zA-Z0-9-\s]*\'|.*[=>])/)
         if (textAfterClassAttributeStart.length > 1 && attributeClosed === -1) {
-            return completionItems;
+            return this.completionItems;
         } else {
             return Promise.resolve([]);
         }
--- src/extension.ts
+++ src/extension.ts
@@ -2,13 +2,13 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import { languages, ExtensionContext } from 'vscode';
-import provider from './cssCompletionItemProvider';
+import { CssCompletionItemProvider } from './cssCompletionItemProvider';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
 
-    let disposable = languages.registerCompletionItemProvider('html', provider);
+    let disposable = languages.registerCompletionItemProvider('html', new CssCompletionItemProvider());
 
     context.subscriptions.push(disposable);
 }
