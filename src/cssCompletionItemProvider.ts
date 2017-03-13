240b57e57814932ac5823951ab639f7e4ab79529
andersea <andersa@atlab.dk>
2017-03-13 21:47:47 +0100

    Experiment with smart completions

--- src/cssCompletionItemProvider.ts
+++ src/cssCompletionItemProvider.ts
@@ -9,12 +9,33 @@ export class CssCompletionItemProvider implements vscode.CompletionItemProvider 
         this.refreshCompletionItems();
     }
 
+    private getLastAttributeValue(classAttributeText: string) {
+        const attributeValueText = classAttributeText.substr(8);
+        if (attributeValueText.lastIndexOf(" ")) {
+            return attributeValueText.substr(attributeValueText.lastIndexOf(" ") + 1);
+        } else {
+            return attributeValueText;
+        }
+    }
+
     public provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken): Thenable<vscode.CompletionItem[]> {
         let lineUntilPosition = document.getText(new vscode.Range(position.with(undefined, 0), position));
         let textAfterClassAttributeStart = lineUntilPosition.substr(lineUntilPosition.lastIndexOf('class='));
         let attributeClosed = textAfterClassAttributeStart.search(/class=(?:\"[a-zA-Z0-9-\s]*\"|\'[a-zA-Z0-9-\s]*\'|.*[=>])/)
         if (textAfterClassAttributeStart.length > 1 && attributeClosed === -1) {
-            return this.completionItems;
+            const attributeValue = this.getLastAttributeValue(textAfterClassAttributeStart);
+            return this.completionItems.then(items => {
+                const filterResult = items.filter(item => item.label.indexOf(attributeValue) !== -1);
+                return filterResult
+                    .map(item => {
+                        let newItem = new vscode.CompletionItem(item.label);
+                        newItem.textEdit = vscode.TextEdit.replace(new vscode.Range(
+                            new vscode.Position(position.line, position.character - attributeValue.length - 1), position
+                        ), item.label);
+
+                        return newItem;
+                    });
+            });
         } else {
             return Promise.resolve([]);
         }
