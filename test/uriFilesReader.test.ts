e29def8b62f7e93d620b34dd53b2fafdc66808fd
andersea <andersa@atlab.dk>
2017-04-13 12:00:13 +0200

    Bring descriptions in line with cssUtil tests

--- test/uriFilesReader.test.ts
+++ test/uriFilesReader.test.ts
@@ -2,7 +2,7 @@ import uriFilesReader from "../src/uriFilesReader";
 import * as assert from "assert";
 import { Uri } from "vscode";
 
-suite("Uri File Reader Tests", () => {
+suite("Test uriFilesReader", () => {
     test("Can't load http protocol", () => {
         const httpsUri = Uri.parse("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
         return uriFilesReader(Promise.resolve([httpsUri]), "utf8").then(data => {
@@ -10,8 +10,5 @@ suite("Uri File Reader Tests", () => {
         }).catch(err => {
             assert(err.code === "ENOENT");
         });
-
-
     });
-
 });
